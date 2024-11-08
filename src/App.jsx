038854b6a063928ee97ad5b6d5f05792f72eb0ae
orihiko1907 <felipeferreiramachado2008@gmail.com>
2024-11-08 15:24:30 -0300

    calculadora

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return(
+    <main>
+      <h1>Calculadora</h1>
+      <input type="nunber" placeholder="insira um numero"/>
+      <input type="nunber" placeholder="ensira um numero"/> 
+      <div>
+      <button>+</button>
+      <button>-</button>
+      <button>x</button>
+      <button>÷</button>
+      </div>
+      
+    </main>
+  )
+}
