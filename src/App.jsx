d76856ac72da037fd394790255f5e8d120df81c9
orihiko1907 <felipeferreiramachado2008@gmail.com>
2024-11-11 15:58:34 -0300

    atualização do site/finalização

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+import "./style.scss"
 export default function App() {
+  const[primeiroValor, setSegundoValor] =useState()
+  //useState é uma variavel de estado As variáveis ​​podem ser usadas para armazenar dados em um programa, como strings, números, objetos JSON ou valores booleanos 
   return(
     <main>
       <h1>Calculadora</h1>
